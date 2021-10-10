7d726a1e7754116708f3b5b81a84707f09dcff9d
thecptwaffles <65668198+thecptwaffles@users.noreply.github.com>
2021-10-10 12:15:06 -0700

    stuff

---
+++ main.js
@@ -0,0 +1,32 @@
+// var, let, const 
+// string, number, boolean
+// concatenation
+
+
+
+//template strings
+
+//Arrays - variables that hold multiple values
+
+const todos = [
+    {
+        id: 1,
+        text: 'take dog for walk',
+        iscompleted: true
+    }
+
+    {
+        id: 1,
+        text: 'eat breakfast',
+        iscompleted: true
+    }
+
+    {
+        id: 1,
+        text: 'play with friends',
+        iscompleted: false
+    }  
+];
+
+
+
