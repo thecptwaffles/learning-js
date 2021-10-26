aad95a94a3265f7d508b41458d5a3eabecb46e27
thecptwaffles <65668198+thecptwaffles@users.noreply.github.com>
2021-10-26 17:38:36 -0600

    cool

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 var box = document.getElementById('square')
 
-
-function revealmessage() {
-    document.getElementById('hiddenmessage').style.display = 'block'
-
-}
-
 function changesquare() {
     if (box.style.background == 'red') {
         box.style.background = 'green'
